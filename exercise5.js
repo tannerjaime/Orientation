17d707f446bcf66cb9293c78ce78fcd6f5a9923d
tannj583 <tannj583@newschool.edu>
2015-08-23 16:58:49 +0000

    bad file

--- exercise5.js
+++ exercise5.js
@@ -1,15 +1,17 @@
-var fs = require('fs');
-var path = require('path');
-var dir = process.argv[2];
-var ext = process.argv[3];
+var fs = require('fs'); // import the fs core module
+var path = require('path'); // import the path core module 
+var dir = process.argv[2]; // get the file path 
+var ext = process.argv[3]; // get the extension to filter for 2nd argument
 
-fs.readdir(dir, function(error, data){
-    if (error) throw error;
+fs.readdir(dir, function(error, data){ // asynchronously read input directory; when finished reading, invoke callback
+    if (error) throw error; // 
     ext = '.' + ext;
     for (var i = 0; i < data.length; i++){
         //console.log(path.extname(data[i]));
-        if(path.extname(data[i]) == ext){
+        if(path.extname(data[i]) == ext){ // if the file extension matches node school's file extension
             console.log(data[i]);
         }
     }
 });
+
+aoisdjfioajsf
