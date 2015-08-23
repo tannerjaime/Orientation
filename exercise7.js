cbe7b7ee1bc93e2969874c73a118169b3a9167d5
tannj583 <tannj583@newschool.edu>
2015-08-23 18:49:29 +0000

    exercise 7

--- exercise7.js
+++ exercise7.js
@@ -1,11 +1,13 @@
-var http = require('http');
+var http = require('http'); //require http module
+var theUrl = process.argv[2];
 
-http.get(process.argv[2], function (response) {
-  response.setEncoding('utf8');
-  response.on('data', function (data) {
-    console.log(data);
-  });
-  response.on('error', function (error) {
-    console.log(error);
-  });
+//use get function 
+http.get(theUrl, function(response) {
+    response.setEncoding('utf8');   //converts data that is streaming to text from binary
+    response.on('data', function(data) {  //while response is happening, data is coming in
+        console.log(data); //logs data as it was coming in
+    });
+    response.on('error', function(error) {
+        console.log(error);
+    });
 })
